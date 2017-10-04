61a573c1a766f3fd5490770230ae1fc612584636
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2017-10-04 16:55:27 +0200

    [Vertical Lines] Implement Vertical Lines + scripting binding

--- raytracer/raytracer/math/functions/vertical-lines-pattern.cpp
+++ raytracer/raytracer/math/functions/vertical-lines-pattern.cpp
@@ -1,17 +1,15 @@
-#include "math/functions/horizontal-lines-pattern.h"
+#include "math/functions/vertical-lines-pattern.h"
 #include "math/functions.h"
 #include <cmath>
 
 using namespace math;
-
-
-Function<bool(const Point2D&)> math::functions::horizontal_lines(double thickness)
+Function<bool(const Point2D&)> math::functions::vertical_lines(double thickness)
 {
 	std::function<bool(const Point2D&)> function = [thickness](const Point2D& p)
 	{
-		auto y = p.y();
+		auto x = p.x();
 
-		return std::abs(y - round(y)) < thickness / 2;
+		return std::abs(x - round(x)) < thickness / 2;
 	};
 
 	return from_lambda<bool, const Point2D&>(function);
