3cb89f85861e9435a36adf9520b435b10392c3eb
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2017-10-04 12:20:24 +0200

    [Ray Tracer v1] Add RTv1 and a demo using it [Ray Tracer v1] Add RTv1 binding to scripting

--- raytracer/raytracer/demos/demos.h
+++ raytracer/raytracer/demos/demos.h
@@ -1,2 +1,3 @@
 #include "demos/basic-sample.h"
 #include "demos/my-demo.h"
+#include "demos/basic-sample-RTv1.h"
--- raytracer/raytracer/scripting/raytracing-module.cpp
+++ raytracer/raytracer/scripting/raytracing-module.cpp
@@ -14,7 +14,8 @@ namespace
 {
     struct RaytracerLibrary
     {
-        RayTracer v0() const { return raytracer::raytracers::v0(); }
+		RayTracer v0() const { return raytracer::raytracers::v0(); }
+		RayTracer v1() const { return raytracer::raytracers::v1(); }
 
         RayTracer v(int version) const
         {
@@ -54,7 +55,8 @@ ModulePtr raytracer::scripting::_private_::create_raytracing_module()
 
     // Expose each member of the library
 #   define BIND(NAME)   module->add(fun(&RaytracerLibrary::NAME), #NAME)
-    BIND(v0);
+	BIND(v0);
+	BIND(v1);
     BIND(v);
 #   undef BIND
 
