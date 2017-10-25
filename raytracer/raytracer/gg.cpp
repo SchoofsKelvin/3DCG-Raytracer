eb2c8f258d3668e5c5336304872c862a4cc2f651
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2017-10-25 12:33:45 +0200

    Copy gg.h and gg.cpp to raytracer project
    Including from another project doesn't really want to work properly

---
+++ raytracer/raytracer/gg.cpp
@@ -0,0 +1,29 @@
+#include "gg.h"
+
+using namespace gg;
+
+void gg::writefile(const std::vector<triangle> *triangles, std::string filename) {
+	std::ofstream file(filename, std::ios::binary);
+	auto amount = triangles->size();
+	file.write((char*)&amount, sizeof(amount));
+	for each (triangle t in *triangles)
+	{
+		file.write((char*)&t, sizeof(triangle));
+	}
+	file.close();
+}
+
+std::vector<triangle> gg::readfile(std::string filename) {
+	std::vector<triangle> triangles;
+	std::ifstream file(filename, std::ios::binary);
+	int amount;
+	file.read((char*)&amount, sizeof(amount));
+	for (int i = 0; i < amount; i++)
+	{
+		triangle t;
+		file.read((char*)&t, sizeof(triangle));
+		triangles.push_back(t);
+	}
+	file.close();
+	return triangles;
+}
